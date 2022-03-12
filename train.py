ab5cbbcdc3049853337b00f4dd880982bf5d43a1
Hanzhao Lin <readme.md@gmail.com>
2022-03-12 15:35:32 -0800

    Fix build

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ from transformers import DistilBertTokenizerFast
 from transformers import DistilBertForQuestionAnswering
 from transformers import AdamW
 from tensorboardX import SummaryWriter
-
+from fewshot_dataset import FewShotDataset
 
 from torch.utils.data import DataLoader
 from torch.utils.data.sampler import RandomSampler, SequentialSampler
